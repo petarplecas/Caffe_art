1f3a2d74288f704ac781b1e36761c9b61653ce54
petarplecas <plecaspetar3@gmail.com>
2025-11-24 22:01:47 +0100

    Optimized pictures and fixed problem with paths

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -8,18 +8,11 @@
     /**
      * Determines the path prefix based on current location
      * Used for loading assets and templates from subfolders
-     * @returns {string} Path prefix ('../' for subfolders, '' for root)
+     * @returns {string} Path prefix (empty since we use <base> tag)
      */
     function getPathPrefix() {
-        const pathDepth = window.location.pathname.split('/').filter(p => p && p.indexOf('.') === -1).length;
-        const pathname = window.location.pathname;
-        const isInSubfolder = pathDepth > 1 ||
-            pathname.includes('/bar') ||
-            pathname.includes('/gallery') ||
-            pathname.includes('/contact') ||
-            pathname.includes('/faq');
-
-        return isInSubfolder ? '../' : '';
+        // With <base href="/Caffe_art/"> tag, all paths are relative to root
+        return '';
     }
 
     /**
